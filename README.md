b724ed37f3534303b746d47671e241003e3bcfe1
lwenke01 <lwenke@gmail.com>
2016-04-21 09:54:22 -0700

    edited readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # word-cloud-generator
+
+###Install
+*using Python 2.7 (does not work with 3.5)
+
+```python
+pip install wordcloud
+```
+```python
+pip install matplotlib
+```
+```python
+pip install numpy
+```
+
+###Add Data and Image in Script
+####Image:
+On line #10, change the path to an image:
+>pipe_mask = np.array(Image.open(path.join("//Users/lisaw/desktop//", "elephant.jpg")))
+
+####Text File:
+On line #14, add a path to the text file:
+>text = open(path.join('//Users/lisaw/desktop/tapper.txt')).read()
+
+###Run Script
+```python
+python word.py
+```
+
+###Output Example
+Used the transcript from the March 12, 2016 GOP debate and an image of an elephant.
+
+Here is what the script changes the image output to:
+
+>Original image:
+
+>![alt text](/Users/lisabisa25/desktop/elephant.jpg "pre-script")
+
+>Output Image:
+
+>![alt text](/Users/lisabisa25/desktop/figure_1.png "post-script")
+
+###Credit
+Stacie Mahuna (smahuna)
---
+++ word.py
@@ -0,0 +1,34 @@
+
+from os import path
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import numpy as np
+from PIL import Image
+
+
+#read in image and its colors - this is optional in case you want to make the word cloud conform to a specific shape
+pipe_mask = np.array(Image.open(path.join("//Users/lisabisa25/desktop//", "elephant.jpg")))
+image_colors = ImageColorGenerator(pipe_mask)
+
+# Read in the whole text
+text = open(path.join('//Users/lisabisa25/desktop/tapper.txt')).read()
+
+#generate word cloud and set specifications - font type, stop words, background, height/width, and set the shape of the word cloud to conform to your picture
+wordcloud_final = WordCloud(
+    # font_path='C:\\Windows\\Fonts\\cour.ttf',
+    stopwords=STOPWORDS.add("P"),
+    background_color='white',
+    width=1800,
+    height=1400,
+    mask=pipe_mask
+).generate(text)
+
+
+# Open a plot of the generated image, using picture and image colors pulled from picture to shape the word cloud
+plt.imshow(wordcloud_final.recolor(color_func=image_colors))
+plt.axis("off")
+plt.figure()
+plt.imshow(pipe_mask
+    , cmap=plt.cm.gray)
+plt.axis("off")
+plt.show()
